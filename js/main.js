40f94be34cddef97b5e99962427246ee82a34321
Gabrielli Michachi <gabrielli.paula@aluno.senai.br>
2026-02-26 11:27:38 -0300

    testando logicas

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// 1. lógica do menu principal
+
+const btnP1 = document.querySelector('#inputP1');
+const btnP2 = document.querySelector('#inputP2');
+const btnP3 = document.querySelector('#inputP3');
+
+// 2. metódo addEventListener com uma função anônima
+
+btnP1.addEventListener('click', function () {
+    window.location.href = './pages/projeto01.html'
+});
+
+btnP2.addEventListener('click', function () {
+    window.location.href = './pages/projeto02.html'
+});
+
+btnP3.addEventListener('click', function () {
+    window.location.href = './pages/projeto03.html'
+});
+
+    function cacho(x, y) {
+    return x + y
+
+    const banana = cacho
+    // arrow function
+    const uva =  () => 'sou uma fruta';
+
+    // exemplo funcao literal
+    function uva() {
+        return('sou uma fruta!!!!')
+    }
+
+
+};
